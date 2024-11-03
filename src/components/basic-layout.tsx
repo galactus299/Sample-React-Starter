220301a4ad2622d89637a521d60811462765bc66
Shayan Babar <mianshayanbabar@gmail.com>
2024-11-03 15:01:03 +0500

    feat: git hub link

--- src/components/basic-layout.tsx
+++ src/components/basic-layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
+
+import  GitHub from "../assets/github-mark.svg";
+
 const BasicLayout = () => {
     return (
         <div>
-            <Outlet />
-
+            <Outlet/>
+            <a href={'https://github.com/galactus299/Sample-React-Starter'}>
+                <img src={GitHub} alt={'github'} className={'fixed bottom-2 right-2  z-50 rounded w-[50px] h-[50px]'}/>
+            </a>
         </div>
     );
 };
